9e8f27eff0c2add3d707b4d6d9ea0069a7780692
Phuong <phuong@candy-macbook>
2016-06-21 20:17:19 +0700

    Initial Commit

---
+++ AutoLayoutExample4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample4
+//
+//  Created by Phuong on 6/21/16.
+//  Copyright © 2016 Thanh Luu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
